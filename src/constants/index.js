f5a0356e60205092d2a8dd93e15175771ceb48f2
CP-Rahul <rahulcp1401@gmail.com>
2024-08-18 23:37:22 +0530

    Updated baseUrl constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:3000/api/v1";
+export const baseUrl = "https://music-player-backend-4zws.onrender.com/api/v1";
 export const options= () => ({
   headers: {
     "x-access-token": localStorage.getItem("authToken"),
